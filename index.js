9a82d143362980e6765d3944c652de5696ce2f9c
Eddie C <eddiec76@gmail.com>
2018-06-10 21:40:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var playArray = [];
   for (let i=0, i<musicians.length, i++) {
-    playArray.push
+    playArray.push("${musicians[i]} plays ${instruments[i]}");
   }
+  return playArray;
 }
